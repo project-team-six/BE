5ad8f877f77ea24ba35698dc9fa4f689331e0b88
kwangkyun kim <kyuni9786@naver.com>
2023-07-29 14:01:29 +0900

    create

---
+++ src/main/java/team6/zzoget/ZzogetApplication.java
@@ -0,0 +1,13 @@
+package team6.zzoget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzogetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzogetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team6/zzoget/ZzogetApplicationTests.java
@@ -0,0 +1,13 @@
+package team6.zzoget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzogetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

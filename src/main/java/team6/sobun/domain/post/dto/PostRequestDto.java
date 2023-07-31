e55eb9e304dfc7f22a2897b67a923d3ab7fec45b
gilgyujeong <jgg7645@naver.com>
2023-07-31 15:15:24 +0900

    -feat user, post 수정

--- src/main/java/team6/sobun/domain/post/dto/PostRequestDto.java
+++ src/main/java/team6/sobun/domain/post/dto/PostRequestDto.java
@@ -4,10 +4,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import team6.sobun.domain.post.entity.Category;
 
+import java.util.Date;
+
 @Getter
 @NoArgsConstructor
 public class PostRequestDto {
     private Category category;
     private String title;
     private String content;
+    private Date transactionStartDate;
+    private Date transactionEndDate;
+    private Date consumerPeriod;
+    private Date PurchaseDate;
 }

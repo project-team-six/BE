4c28a735610f0ce7740eaa72d25d307d8927ea13
kkamjjing-i <kkamjjingi0111@gmail.com>
2023-08-01 20:20:44 +0900

    이미지 파일 리사이징 1차

--- src/main/java/team6/sobun/SobunApplication.java
+++ src/main/java/team6/sobun/SobunApplication.java
@@ -5,6 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// 이미지 리사이징 라이브러리 사용 시 , 이미지IO와 변환 과정에서 3mb 이상 이미지는 리사이징하는 데 상당히 오랜 시간이 소요된다.
+// 따라서 아래와 같이 multipart 사이즈에 제약 조건을 설정한다.
+//spring.servlet.multipart.max-file-size=3MB
+//spring.servlet.multipart.max-request-size=3MB
+
+// yaml 형식
+//        spring:
+//        servlet:
+//        multipart:
+//        max-file-size: 3MB
+//        max-request-size: 3MB
+
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableCaching

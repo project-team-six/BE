84649ec4f0d7fea043b6df45bc1687412eb2a9a3
kwangkyun kim <kyuni9786@naver.com>
2023-08-15 16:18:31 +0900

    병합오류 해결

--- src/main/java/team6/sobun/domain/post/dto/PostResponseDto.java
+++ src/main/java/team6/sobun/domain/post/dto/PostResponseDto.java
@@ -93,7 +93,7 @@ public class PostResponseDto {
 
     // 유저 조회 시에 사용하는 생성자
 
-    public PostResponseDto(Long id, Long userId, Category category,String status, String title, String nickname, String content, LocalDateTime createdAt, List<String> imageUrlList, long pined, int views, String transactionStartDate, String transactionEndDate, String consumerPeriod, String purchaseDate, String location, String price) {
+    public PostResponseDto(Long id, Long userId, Category category, String status, String title, String nickname, String content, LocalDateTime createdAt, List<String> imageUrlList, long pined, int views, String transactionStartDate, String transactionEndDate, String consumerPeriod, String purchaseDate, String location, String price) {
         this.id = id;
         this.userId = userId;
         this.category = category;
@@ -114,4 +114,5 @@ public class PostResponseDto {
         this.status = status;
 
     }
+}
 

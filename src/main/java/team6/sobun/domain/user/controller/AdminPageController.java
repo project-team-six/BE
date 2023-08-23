0c4f9a8de2734da1557dcd6dbdc47b227bf970f9
gilgyujeong <jgg7645@naver.com>
2023-08-24 03:06:02 +0900

    feat:관리자페이지 유저조회 및 정지유저 조회

---
+++ src/main/java/team6/sobun/domain/user/controller/AdminPageController.java
@@ -0,0 +1,42 @@
+package team6.sobun.domain.user.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team6.sobun.domain.user.entity.UserRoleEnum;
+import team6.sobun.domain.user.service.AdminPageService;
+import team6.sobun.global.responseDto.ApiResponse;
+import team6.sobun.global.responseDto.ErrorResponse;
+import team6.sobun.global.security.UserDetailsImpl;
+import team6.sobun.global.stringCode.ErrorCodeEnum;
+
+@Tag(name = "관리자 페이지 관련 API", description = "관리자 페이지 관련")
+@RestController
+@RequiredArgsConstructor
+public class AdminPageController {
+
+    private final AdminPageService adminPageService;
+
+    @Operation(summary = "전체 유저 조회")
+    @GetMapping("/admin/userList")
+    public ApiResponse<?> userList(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        if (userDetails.getUser().getRole() == UserRoleEnum.ADMIN) {
+            return adminPageService.userList();
+        } else {
+            return ApiResponse.error(new ErrorResponse(ErrorCodeEnum.NO_PERMISSIONS));
+        }
+    }
+
+    @Operation(summary = "권한정지 유저 조회")
+    @GetMapping("/admin/blackUser")
+    public ApiResponse<?> blackUserList(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        if (userDetails.getUser().getRole() == UserRoleEnum.ADMIN) {
+            return adminPageService.userBlackList();
+        } else {
+            return ApiResponse.error(new ErrorResponse(ErrorCodeEnum.NO_PERMISSIONS));
+        }
+    }
+}
--- src/main/java/team6/sobun/domain/user/dto/UserResponseDto.java
+++ src/main/java/team6/sobun/domain/user/dto/UserResponseDto.java
@@ -9,12 +9,14 @@ import java.util.List;
 
 @Getter
 public class UserResponseDto {
+    private Long id;
     private String email;
     private String username;
     private String profileImageUrl;
     private UserRoleEnum role;
 
     public UserResponseDto(User user) {
+        this.id = user.getId();
         this.email = user.getEmail();
         this.username = user.getNickname();
         this.profileImageUrl = user.getProfileImageUrl();
